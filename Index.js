63d829f70d1bd0cf308467d5b383066c8eb8da6d
saburabrina <sabrinascosme@gmail.com>
2020-03-07 15:51:42 -0300

    movimentação não variável pronta

--- Index.js
+++ Index.js
@@ -1,17 +1,14 @@
 function iniciar_jogo(){
 	canvas.foodLength = 1;
 	canvas.snakeLength = 5;	
-	canvas.snakePosition = [];	
-	canvas.snakeStep = 2; 	
-	canvas.snakeDirection = 1; // x	
-	canvas.snakeSense = 1; // up or left	
+	canvas.snakeStep = 2;	
 	canvas.food();	
 	canvas.snake();	
-	//canvas.movement();
+	canvas.movement();
 }
 
 var canvas = {	
-	myself :  document.getElementById('campo'),	
+	myself : document.getElementById('campo'),	
 	myCtx : document.getElementById('campo').getContext('2d'),	
 	food : function (){		
 		var x = randomize(0, this.foodLength);		
@@ -20,26 +17,37 @@ var canvas = {
 		this.myCtx.fillRect(x, y, 5, 5);	
 	},	
 	snake: function(){		
-		var XorY = randomizeBool();		
-		var x = randomize(XorY, this.snakeLength);		
-		var y = randomize(1-XorY, this.snakeLength);
-		this.myCtx.fillStyle = "yellow";		
-		for(var i = 0; i < this.snakeLength*5; i+=5){			
-			this.myCtx.fillRect(x + i*XorY, y + i*(1-XorY), 5, 5);			
-			this.snakePosition.push({x: x + i*XorY, y: y + i*(1-XorY)});		
-		}	
-	} //,	
-	/*movement : function(){		
-		setInterval(()=>{			
-			if(this.snakeDirection){				
-				snakePosition.unshift({x : })			
-			} 		
-		},1000);	
-	}*/
+		this.snakeDirection = randomizeBool(); // #t: X	
+		var X = randomize(this.snakeDirection, this.snakeLength);		
+		var Y = randomize(1-this.snakeDirection, this.snakeLength);
+
+		var width = this.snakeDirection ? this.snakeLength : 1;
+		var height = this.snakeDirection ? 1 : this.snakeLength;
+		
+		this.myCtx.fillStyle = "yellow";
+		this.myCtx.fillRect(X, Y, 5*width, 5*height);			
+		this.snakePosition = {x: X, y: Y};		
+	},	
+	movement : function(){
+		return setInterval(()=>{
+			var X = this.snakePosition.x;
+			var Y = this.snakePosition.y;
+
+			var width = this.snakeDirection ? this.snakeLength : 1;
+			var height = this.snakeDirection ? 1 : this.snakeLength;
+
+			this.myCtx.clearRect(X, Y, 5*width, 5*height);
+			this.myCtx.fillRect(X - (this.snakeDirection)*5*this.snakeStep, Y - (1 - this.snakeDirection)*5*this.snakeStep, 5*width, 5*height);
+
+			this.snakePosition = {x: X - (this.snakeDirection)*5*this.snakeStep, y: Y - (1 - this.snakeDirection)*5*this.snakeStep};
+
+			//if(this.snakePosition.x == 0 || this.snakePosition.y == 0)
+		},1000);
+	}
 }
 
 function randomizeBool() {	
-	var choice = Math.random();	
+	var choice = Math.random();
 	choice = choice > 0.5 ? 1 : 0;	
 	return choice;
 }
